af94cb0617519d16f17700b139ff50b1d4d66099
Justus Soh <justus.soh@hotmail.com>
2019-10-08 18:25:02 +0800

    Build Commit

---
+++ client/build/precache-manifest.041925beec8574d677004f87cb287113.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "075898fde6c9a12a5b7549170c5773bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7034f241eaa2c6a9d12b",
+    "url": "/static/css/main.5e4e9510.chunk.css"
+  },
+  {
+    "revision": "138ce3a88c1e935ed16a",
+    "url": "/static/js/2.87a2e41a.chunk.js"
+  },
+  {
+    "revision": "7034f241eaa2c6a9d12b",
+    "url": "/static/js/main.d4503fc5.chunk.js"
+  },
+  {
+    "revision": "f1c6019deaec6f4a9d0b",
+    "url": "/static/js/runtime-main.11bc70d3.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
