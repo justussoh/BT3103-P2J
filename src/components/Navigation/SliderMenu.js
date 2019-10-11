00a36ea493d6ee71f1e21e730634a1c6bc45e5db
Shao Yang <hongsy2006@gmail.com>
2019-10-11 18:36:07 +0800

    Use listQuestions

--- src/components/Navigation/SliderMenu.js
+++ src/components/Navigation/SliderMenu.js
@@ -1,5 +1,5 @@
-import {push as Menu} from 'react-burger-menu';
-import React, {Component} from 'react';
+import { push as Menu } from 'react-burger-menu';
+import React, { Component } from 'react';
 import CloseIcon from '@material-ui/icons/Close'
 import IconButton from "@material-ui/core/IconButton";
 import Grid from "@material-ui/core/Grid";
@@ -10,22 +10,32 @@ import './SliderMenu.css'
 class SliderMenu extends Component {
 
     render() {
+        const questions = [
+            '01. Question',
+            '02. Question'
+        ]
+
+        const listQuestions = questions.map((q, i) =>
+            <Grid item xs={12} onClick={() => this.props.handleClickQuestion(i)}>
+                {q}
+            </Grid>
+        );
+
+
         return (
             <Menu pageWrapId={"page-wrap"} outerContainerId={"root"}
-                  customBurgerIcon={false} isOpen={this.props.open}
-                  onStateChange={(state) => this.props.handleMenuStateChange(state)}
-                  customCrossIcon={false} className='menu-width'
+                customBurgerIcon={false} isOpen={this.props.open}
+                onStateChange={(state) => this.props.handleMenuStateChange(state)}
+                customCrossIcon={false} className='menu-width'
             >
                 <Grid container spacing={3}
                 >
                     <Grid item xs={12} className='d-flex align-items-center'>
                         <IconButton className='closeButtonMenu ml-auto' onClick={this.props.handleCloseMenu}>
-                            <CloseIcon className='closeIconMenu'/>
+                            <CloseIcon className='closeIconMenu' />
                         </IconButton>
                     </Grid>
-                    <Grid item xs={12} onClick={(e)=>this.props.handleClickQuestion()}>
-                        01. Question
-                </Grid>
+                    {listQuestions}
                 </Grid>
 
             </Menu>
