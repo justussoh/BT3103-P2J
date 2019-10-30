895ef5b67831adb305d12ecc9ffa610e5dc25a67
Shao Yang <hongsy2006@gmail.com>
2019-10-30 18:29:08 +0800

    Hide keys from repo

--- src/util/firebase.tsx
+++ src/util/firebase.tsx
@@ -2,15 +2,5 @@ import * as firebase from 'firebase';
 import "firebase/auth";
 import "firebase/database";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyCyqp3-1_RgOzY693ucdrQMbK2WIcIUUZk",
-    authDomain: "pythontojavascript.firebaseapp.com",
-    databaseURL: "https://pythontojavascript.firebaseio.com",
-    projectId: "pythontojavascript",
-    storageBucket: "pythontojavascript.appspot.com",
-    messagingSenderId: "69240043646",
-    appId: "1:69240043646:web:1d62b420eda2e45c88802f",
-    measurementId: "G-9PH2BQQCT4"
-};
-
+var firebaseConfig = require('./firebaseConfig.json');
 export const firebaseApp = firebase.initializeApp(firebaseConfig);
