32a7351f5436a3b533d9976c9267bff7bec0b125
Shao Yang <hongsy2006@gmail.com>
2019-10-31 09:32:01 +0800

    Format PastAnswers

--- src/components/PastAnswers/PastAnswers.tsx
+++ src/components/PastAnswers/PastAnswers.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {QuestionIface} from "../Form/Question";
+import { QuestionIface } from "../Form/Question";
 import AceEditor from "react-ace";
 import MenuItem from '@material-ui/core/MenuItem';
 import FormControl from '@material-ui/core/FormControl';
@@ -8,7 +8,7 @@ import Select from '@material-ui/core/Select';
 import 'brace/mode/javascript';
 import 'brace/theme/monokai';
 import Paper from "@material-ui/core/Paper";
-import {Row, Col, Container} from "react-bootstrap";
+import { Row, Col, Container } from "react-bootstrap";
 
 type MyProps = {
     questions: QuestionIface[],
@@ -22,56 +22,59 @@ class PastAnswers extends React.Component<MyProps, {}> {
     };
 
     handlePastAnswerSelect = (e: any) => {
-        this.setState({showAnswer: e.target.value})
+        this.setState({ showAnswer: e.target.value })
     };
 
     render() {
         const questions = this.props.questions;
         const currQ = this.props.question;
+
+        if (!questions[currQ].pastAnswers || questions[currQ].pastAnswers.length === 0) {
+            return (<h6>You have not submitted any answer yet.</h6>)
+        }
+
         return (
-            <div className='d-flex align-items-center justify-content-center flex-column' style={{minHeight: '50vh'}}>
-                <Paper className='mr-auto'>
-                    <FormControl style={{padding: '8px 16px'}}>
-                        <Select value={this.state.showAnswer} onChange={this.handlePastAnswerSelect}>
-                            {questions[currQ].pastAnswers.map((ans, index) => {
-                                return (
-                                    <MenuItem value={index}>{index + 1}. Attempt {index + 1}</MenuItem>
-                                );
-                            })}
-                        </Select>
-                    </FormControl>
-                </Paper>
-                {questions[currQ].pastAnswers.length > 0 ?
-                    <Container fluid>
-                        <Row className='d-flex align-items-center justify-content-center'
-                             style={{marginTop: '25px'}}>
-                            <Col xs={6}>
-                                <AceEditor
-                                    wrapEnabled
-                                    height='50vh'
-                                    width='100%'
-                                    mode="javascript"
-                                    theme="monokai"
-                                    name="info-section"
-                                    tabSize={0}
-                                    editorProps={{
-                                        $blockScrolling: true,
-                                    }}
-                                    value={questions[currQ].pastAnswers[this.state.showAnswer].pastAnswer as string}
-                                    style={{maxWidth: 570}}
-                                />
-                            </Col>
-                            <Col xs={6}>
-                                <Paper>
-                                    <div
-                                        dangerouslySetInnerHTML={{__html: questions[currQ].pastAnswers[this.state.showAnswer].errorMessage}}/>
-                                </Paper>
-                            </Col>
-                        </Row>
-                    </Container>
-                    :
-                    <h6>You have not submitted any answer yet.</h6>}
-            </div>
+            <Container fluid>
+                <Row>
+                    <Col lg="auto" md="auto">
+                        <Paper>
+                            <FormControl>
+                                <Select value={this.state.showAnswer} onChange={this.handlePastAnswerSelect}>
+                                    {questions[currQ].pastAnswers.map((ans, index) => {
+                                        return (
+                                            <MenuItem value={index}>{index + 1}. Attempt {index + 1}</MenuItem>
+                                        );
+                                    })}
+                                </Select>
+                            </FormControl>
+                        </Paper>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <AceEditor
+                            wrapEnabled
+                            height='50vh'
+                            width='100%'
+                            mode="javascript"
+                            theme="monokai"
+                            name="info-section"
+                            tabSize={0}
+                            editorProps={{
+                                $blockScrolling: true,
+                            }}
+                            value={questions[currQ].pastAnswers[this.state.showAnswer].pastAnswer as string}
+                            style={{ maxWidth: 570 }}
+                        />
+                    </Col>
+                    <Col lg={6} md={6}>
+                        <Paper>
+                            <div
+                                dangerouslySetInnerHTML={{ __html: questions[currQ].pastAnswers[this.state.showAnswer].errorMessage }} />
+                        </Paper>
+                    </Col>
+                </Row>
+            </Container>
         );
     }
 }
