05e0af7df7bfb0f40a3c9a9d49ee2148340d07b7
Shao Yang <hongsy2006@gmail.com>
2019-11-05 16:51:29 +0800

    Fix bug that showed incorrect alert

--- src/components/Form/Alert.tsx
+++ src/components/Form/Alert.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import {Col, Row, Alert} from "react-bootstrap";
-import {QuestionIface} from "./Question";
-import {firebaseApp} from "../../util/firebase";
+import { Col, Row, Alert } from "react-bootstrap";
+import { QuestionIface } from "./Question";
+import { firebaseApp } from "../../util/firebase";
 
 type MyProps = {
     question: number,
@@ -9,7 +9,7 @@ type MyProps = {
     handleAlertClose: () => void,
 };
 
-function getRandomInt(max:number) {
+function getRandomInt(max: number) {
     return Math.floor(Math.random() * Math.floor(max));
 }
 
@@ -17,48 +17,44 @@ class CustomAlert extends React.Component<MyProps, {}> {
 
     state = {
         data: {
-            correctAnswer:0,
-            totalTries:0,
-            wrongAnswer:0
+            correctAnswer: 0,
+            totalTries: 0,
+            wrongAnswer: 0
         },
         errorAlert: ["Please try again! You can use the hints if you need more help."],
         successAlert: ["You answered the question correctly! Please move on to the next question."],
     };
 
     componentDidMount(): void {
+        console.log("componentDidMount");
         let db = firebaseApp.database().ref(`/logging/${this.props.question}`);
         db.once('value').then((snapshot) => {
             let data = snapshot.val();
-            console.log(data);
             if (data !== null) {
-                this.setState({data: data})
+                this.setState({ data: data })
+                console.log("retrieved alert data from db")
             }
         }).catch(err => {
             console.error(err);
         });
-        console.log(this.props)
     };
 
-    renderText = (pass:boolean) =>{
-        if (pass){
-            let successAlert = this.state.errorAlert;
-            let text1 = `Congratulations, you are part of the ${Math.floor(this.state.data.correctAnswer)} people who got the question correct!`;
-            successAlert.push(text1);
-            let randInt = getRandomInt(successAlert.length);
-            return successAlert[randInt];
-        }else{
+    renderText = (pass: boolean) => {
+        if (pass) {
+            let successAlert = this.state.successAlert;
+            successAlert.push(`Congratulations, you are part of the ${Math.floor(this.state.data.correctAnswer)} people who got the question correct!`);
+            return successAlert[getRandomInt(successAlert.length)];
+        } else {
             let errorAlert = this.state.errorAlert;
-            let text1 = `Don't be disheartened this is one of our more difficult questions, only ${Math.floor((this.state.data.correctAnswer/this.state.data.totalTries)*100)}% got the question correct!`
-            errorAlert.push(text1);
-            let randInt = getRandomInt(errorAlert.length);
-            return errorAlert[randInt];
+            const percentage = Math.floor((this.state.data.correctAnswer / this.state.data.totalTries) * 100);
+            errorAlert.push(`Don't be disheartened this is one of our more difficult questions, only ${percentage}% got the question correct!`);
+            return errorAlert[getRandomInt(errorAlert.length)];
         }
     };
 
-
     render() {
         return (
-            <Row className='d-flex align-items-center justify-content-center' style={{marginBottom: 10}}>
+            <Row className='d-flex align-items-center justify-content-center' style={{ marginBottom: 10 }}>
                 <Col xs={10}>
                     <Alert
                         variant={this.props.questions[this.props.question].completed ? "success" : "danger"}
