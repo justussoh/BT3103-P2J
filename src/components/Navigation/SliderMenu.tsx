63d2fd383fd752193f5842cfc27e65889903b12a
Justus Soh <justus.soh@hotmail.com>
2019-11-13 23:28:16 +0800

    Added Home Page

--- src/components/Navigation/SliderMenu.tsx
+++ src/components/Navigation/SliderMenu.tsx
@@ -1,12 +1,12 @@
 import Grid from "@material-ui/core/Grid";
 import React from 'react';
-import { push as Menu, State } from "react-burger-menu";
+import {push as Menu, State} from "react-burger-menu";
 import './SliderMenu.css';
 import Button from "@material-ui/core/Button";
 import Divider from '@material-ui/core/Divider';
-import { QuestionIface } from "../Form/Question";
-import { withRouter } from 'react-router-dom';
-import { RouteComponentProps } from "react-router";
+import {QuestionIface} from "../Form/Question";
+import {withRouter} from 'react-router-dom';
+import {RouteComponentProps} from "react-router";
 
 type MyProps = RouteComponentProps & {
     open: boolean,
@@ -18,7 +18,7 @@ type MyProps = RouteComponentProps & {
     handleClickQuestion: (arg0: number) => void,
     handleMenuStateChange: (arg0: State) => void,
     handleSaveState: () => void,
-    handleLoadState: (arg0:boolean) => void,
+    handleLoadState: (arg0: boolean) => void,
     handleStart: () => void,
     toggleAdmin: () => void,
 };
@@ -34,70 +34,41 @@ class SliderMenu extends React.Component<MyProps, {}> {
 
         return (
             <Menu pageWrapId={"page-wrap"} outerContainerId={"root"}
-                isOpen={this.props.open}
-                onStateChange={(state) => this.props.handleMenuStateChange(state)}
-                className='menu-width'
+                  isOpen={this.props.open}
+                  onStateChange={(state) => this.props.handleMenuStateChange(state)}
+                  className='menu-width'
             >
-                {this.props.question === 0 || this.props.question === (this.props.questions.length)?
-                    <Grid container spacing={3}>
-                        <Grid item xs={12} className='d-flex align-items-center justify-content-center'>
-                            <Button variant="outlined" className='button-start' size='large'
-                                onClick={this.props.handleStart}>
-                                START
-                            </Button>
-                        </Grid>
-                        <Grid item xs={12} className='d-flex align-items-center justify-content-center'>
-
-                            <Button variant="outlined" className='button-start'
-                                onClick={this.handleResume}>
-                                RESUME
-                                </Button>
-
-                        </Grid>
-                        <Grid item xs={12} className='d-flex align-items-center justify-content-center'>
-                            <Button variant="outlined" className='button-start'
-                                onClick={this.props.toggleAdmin}>
-                                ADMIN MODE</Button>
-                        </Grid>
-                        <Grid item xs={12}>
-                            <Divider variant='middle' style={{ backgroundColor: '#fff' }} />
-                        </Grid>
-                        <Grid item xs={12}>
-                            <span style={{ color: 'white' }}>
-                                From Python to JS is a quick and easy online module will teach you JavaScript, the popular programming language used for the Web.
-                                </span>
-                        </Grid>
-                    </Grid>
-                    :
+                {this.props.question > 0 && this.props.question < this.props.questions.length ?
                     <Grid container spacing={2} className='h-100'>
                         <Grid item xs={12}>
-                            <span style={{ color: 'white' }}>
+                            <span style={{color: 'white'}}>
                                 Are you feeling lost? Need some help? Here are some resources to help you learn.
                             </span>
                         </Grid>
                         <Grid item xs={12}>
-                            <span style={{ color: 'white' }}>
+                            <span style={{color: 'white'}}>
                                 <a href='https://www.w3schools.com/js/' target='_blank' rel="noopener noreferrer">01. W3 Schools</a>
                             </span>
                         </Grid>
                         <Grid item xs={12}>
-                            <span style={{ color: 'white' }}>
-                                <a href='https://developer.mozilla.org/en-US/docs/Web/JavaScript' target='_blank' rel="noopener noreferrer">02. MDN Web Documents</a>
+                            <span style={{color: 'white'}}>
+                                <a href='https://developer.mozilla.org/en-US/docs/Web/JavaScript' target='_blank'
+                                   rel="noopener noreferrer">02. MDN Web Documents</a>
                             </span>
                         </Grid>
                         <Grid item xs={12}>
-                            <span style={{ color: 'white' }}>Check this video out for a quick introduction</span>
+                            <span style={{color: 'white'}}>Check this video out for a quick introduction</span>
                             <iframe width="100%" height="auto" src="https://www.youtube.com/embed/W6NZfCO5SIk"
                                     frameBorder="0"
                                     allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                                    allowFullScreen />
+                                    allowFullScreen/>
                         </Grid>
 
                         <Grid item xs={12}>
-                            <Divider variant='middle' style={{ backgroundColor: '#fff' }} />
+                            <Divider variant='middle' style={{backgroundColor: '#fff'}}/>
                         </Grid>
                         <Grid item xs={12}>
-                            <span style={{ color: 'white' }}>
+                            <span style={{color: 'white'}}>
                                 Want to continue some other time? Simply key in a unique key and save!
                             </span>
                         </Grid>
@@ -105,7 +76,9 @@ class SliderMenu extends React.Component<MyProps, {}> {
                         <Grid item xs={12}>
                             <label className="field a-field a-field_a3">
                                 <input className="field__input a-field__input" value={this.props.userID}
-                                    onChange={(e) => { this.props.onUserIDChange(e.target.value); }}
+                                       onChange={(e) => {
+                                           this.props.onUserIDChange(e.target.value);
+                                       }}
                                 />
                                 <span className="a-field__label-wrap">
                                     <span className="a-field__label">Enter User ID</span>
@@ -113,14 +86,43 @@ class SliderMenu extends React.Component<MyProps, {}> {
                             </label>
                             <div className='d-flex'>
                                 <Button variant="outlined" className='button-start' size='large'
-                                    onClick={this.props.handleSaveState}>
+                                        onClick={this.props.handleSaveState}>
                                     SAVE
                                 </Button>
                                 <Button variant="outlined" className='button-start ml-auto' size='large'
-                                    onClick={()=>this.props.handleLoadState(true)}>
+                                        onClick={() => this.props.handleLoadState(true)}>
                                     LOAD
                                 </Button>
                             </div>
+                        </Grid>
+                    </Grid> :
+                    <Grid container spacing={3}>
+                        <Grid item xs={12} className='d-flex align-items-center justify-content-center'>
+                            <Button variant="outlined" className='button-start' size='large'
+                                    onClick={this.props.handleStart}>
+                                START
+                            </Button>
+                        </Grid>
+                        <Grid item xs={12} className='d-flex align-items-center justify-content-center'>
+
+                            <Button variant="outlined" className='button-start'
+                                    onClick={this.handleResume}>
+                                RESUME
+                            </Button>
+
+                        </Grid>
+                        <Grid item xs={12} className='d-flex align-items-center justify-content-center'>
+                            <Button variant="outlined" className='button-start'
+                                    onClick={this.props.toggleAdmin}>
+                                ADMIN MODE</Button>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <Divider variant='middle' style={{backgroundColor: '#fff'}}/>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <span style={{color: 'white'}}>
+                                From Python to JS is a quick and easy online module will teach you JavaScript, the popular programming language used for the Web.
+                                </span>
                         </Grid>
                     </Grid>
                 }
