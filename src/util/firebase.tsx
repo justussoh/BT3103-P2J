48c7634897b1c530afa3be3d3e06e15e7b4e9ac6
justussoh <45728710+justussoh@users.noreply.github.com>
2019-11-14 14:06:09 +0000

    Deploying to gh-pages from master 4d570634638007f204390cd01b4f42786eb8855d

--- src/util/firebase.tsx
+++
@@ -1,6 +0,0 @@
-import * as firebase from 'firebase';
-import "firebase/auth";
-import "firebase/database";
-
-var firebaseConfig = require('./firebaseConfig.json');
-export const firebaseApp = firebase.initializeApp(firebaseConfig);
